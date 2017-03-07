129644fac733d03a5da21ffc647869fbcd5625cf
Tim Elvart <telvart@ku.edu>
2017-03-07 16:14:12 -0600

    Add column names

--- project2/main.cpp
+++ project2/main.cpp
@@ -1,7 +1,18 @@
 
 #include <iostream>
+#include <string>
+
+std::string columnLetters[] =
+{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+ "AA","AB","AC","AD","AE","AF","AG","AH","AI","AJ","AK","AL","AM","AN","AO","AP","AQ","AR","AS","AT","AU","AV","AW","AX","AY","AZ"
+ "BA","BB","BC","BD","BE","BF","BG","BH","BI","BJ","BK","BL","BM","BN","BO","BP","BQ","BR","BS","BT","BU","BV","BW","BX","BY","BZ"
+ "CA","CB","CC","CD","CE","CF","CG","CH","CI","CJ","CK","CL","CM","CN","CO","CP","BQ","CR","CS","CT","CU","CV","CW","CX","CY","CZ"
+ "DA","DB","DC","DD","DE","DF","DG","DH","DI","DJ","DK","DL","DM","DN","DO","DP","DQ","DR","DS","DT","DU","DV","DW","DX","DY","DZ"};
+
+
 
 int main(int argc, char** argv)
 {
-  std::cout<<"Hello, Project 2!";
+
+  std::cout<<4*26;
 }
