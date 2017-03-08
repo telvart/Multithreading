93f9ed38b91925b06f4ccfa43a63c8ad3266f311
Tim Elvart <telvart@ku.edu>
2017-03-08 12:09:52 -0600

    change makefile for mpic++

--- project2/main.cpp
+++ project2/main.cpp
@@ -1,13 +1,14 @@
 
 #include <iostream>
 #include <string>
+#include <mpi.h>
 
 std::string columnLetters[] =
 {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
  "AA","AB","AC","AD","AE","AF","AG","AH","AI","AJ","AK","AL","AM","AN","AO","AP","AQ","AR","AS","AT","AU","AV","AW","AX","AY","AZ"
  "BA","BB","BC","BD","BE","BF","BG","BH","BI","BJ","BK","BL","BM","BN","BO","BP","BQ","BR","BS","BT","BU","BV","BW","BX","BY","BZ"
  "CA","CB","CC","CD","CE","CF","CG","CH","CI","CJ","CK","CL","CM","CN","CO","CP","BQ","CR","CS","CT","CU","CV","CW","CX","CY","CZ"
- "DA","DB","DC","DD","DE","DF","DG","DH","DI","DJ","DK","DL","DM","DN","DO","DP","DQ","DR","DS","DT","DU","DV","DW","DX","DY","DZ"};
+ "DA","DB","DC","DD","DE","DF","DG","DH","DI","DJ","DK","DL","DM"};
 
 
 
@@ -15,4 +16,5 @@ int main(int argc, char** argv)
 {
 
   std::cout<<4*26;
+  return 0;
 }
